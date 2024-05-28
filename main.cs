3d7b033b767d686d9c737a5c32d85e7dd6c8bd4b
ShulhaCorporation <166990864+ShulhaCorporation@users.noreply.github.com>
2024-05-28 19:57:23 +0300

    Program that calculates weekday of any date

--- main.cs
+++ main.cs
@@ -1,30 +1,154 @@
-public class Starting
-{
-    public Starting()
-    {
-
+using System;
+class HelloWorld {
+    static int year;
+    static int month;
+    static int day;
+    static bool isLeap;
+    static int monthCode;
+    static int centuryCode;
+    static int yearCode;
+    static int weekDay;
+  static void Main() {
+    Console.WriteLine("Калькулятор дня тижня за датою");
+    
+    Console.WriteLine("Введіть рік");
+    int.TryParse(Console.ReadLine(), out year);
+    isLeap = LeapCheck();
+    
+    monthMark:
+    
+    Console.WriteLine("Введіть число місяця");
+    int.TryParse(Console.ReadLine(), out month);
+    
+    if(month < 1 || month > 12){
+        Console.WriteLine("Такого місяця не існує");
+        goto monthMark;
     }
-    static void Main(string[] args)
-    {
-        Console.WriteLine("СВИНЯ");
-        Console.WriteLine("Введіть s, щоб розпочати");
-        Console.WriteLine("        r - правила гри та команди");
-    mark:
-        string choise = Console.ReadLine();
-        if (choise.Equals("r"))
-        {
-            Console.Write("Ви граєте проти комп'ютера. Під час свого ходу ви можете кинути кубик(введіть d),\n або записати очки і передати хід супернику(введіть pass).");
-            Console.WriteLine("Якщо під час кидка кості випало значення від 2 до 6, воно додається в малий рахунок. Гравець може кидати кості доти, доки він не зупиниться або поки не випаде 1.\n Якщо випала 1, малий рахунок обнулюється і гравець отримує 0 балів за весь хід. Хід передається наступному гравцеві.");
-            Console.WriteLine("Якщо гравець вирішує зупинитися, очки з малого рахунку зараховуються у загальний і хід передається супернику.\n Гра завершується, коли один з учасників набирає 100 і більше очок в загальному рахунку.");
-            Console.WriteLine("Команди та чіти: limit <натуральне число без дужок> - встановити кількість очок для перемоги. Доступна лише в меню");
-            Console.WriteLine("                  me <натуральне число без дужок> - змінити ваш загальний рахунок (чіти). Доступна лише під час гри");
-            Console.WriteLine("                  bot <натуральне число без дужок> - змінити загальний рахунок бота (чіти). Доступна лише під час гри");
-            goto mark;
-        }
-        if (choise.Equals("s"))
-        {
-            GameManager gm = new GameManager();
-            gm.Startin();
+    
+    dayMark:
+    Console.WriteLine("Введіть день");
+    int.TryParse(Console.ReadLine(), out day);
+    if(day < 1 || day > 31){
+         Console.WriteLine("Такого дня не існує");
+         goto dayMark;
+    }
+    if(day == 31){
+        if(month == 2 || month == 4 || month == 6 || month == 9 || month == 11){
+        Console.WriteLine("Такого дня не існує");
+         goto dayMark;
         }
     }
+    else if(day > 28 && month == 2 && !isLeap){
+        Console.WriteLine("Такого дня не існує");
+         goto dayMark;
+    }
+    else if(day > 29 && month == 2 && isLeap){
+        Console.WriteLine("Такого дня не існує");
+         goto dayMark;
+    }
+    
+     MonthCoding();
+     YearCoding();
+     WeekCounting();
+  }
+  static bool LeapCheck(){
+      
+        if(year % 4 == 0){
+        if(year % 100 == 0 && year % 400 != 0){
+            return false;
+        }
+        else if (year % 400 == 0){
+           return true;
+        }
+        else{
+          return true;
+        }
+    }
+    else{
+       return false;
+    }
+  }
+  static void YearCoding(){
+      CenturyCoding();
+      int lastDigits = year % 100;
+      int almostYearCode = centuryCode + lastDigits + lastDigits/4;
+      yearCode = almostYearCode % 7;
+  }
+  static void CenturyCoding(){
+       int century = year / 100;
+      if(century % 4 == 0){
+          centuryCode = 6;
+      }
+       if(century % 4 == 1){
+          centuryCode = 4;
+      }
+       if(century % 4 == 2){
+          centuryCode = 2;
+      }
+       if(century % 4 == 3){
+          centuryCode = 0;
+      }
+  }
+  static void MonthCoding(){
+      if(month == 1 && !isLeap){
+          monthCode = 1;
+      }
+      if(month == 1 && isLeap){
+          monthCode = 0;
+      }
+      if(month == 2 && !isLeap){
+          monthCode = 4;
+      }
+      if(month == 2 && isLeap){
+          monthCode = 3;
+      }
+      if(month == 3 || month == 11){
+          monthCode = 4;
+      }
+      if(month == 4 || month == 7){
+          monthCode = 0;
+      }
+      if(month == 5){
+          monthCode = 2;
+      }
+       if(month == 6){
+          monthCode = 5;
+      }
+       if(month == 8){
+          monthCode = 3;
+      }
+       if(month == 9 || month == 12){
+          monthCode = 6;
+      }
+      if(month == 10){
+          monthCode = 1;
+      }
+  }
+  static void WeekCounting(){
+      int almostWeekDay = day + monthCode + yearCode;
+      weekDay = almostWeekDay % 7;
+      if(weekDay == 0){
+          Console.WriteLine("Субота");
+      }
+      if(weekDay == 1){
+          Console.WriteLine("Неділя");
+      }
+      if(weekDay == 2){
+          Console.WriteLine("Понеділок");
+      }
+      if(weekDay == 3){
+          Console.WriteLine("Вівторок");
+      }
+      if(weekDay == 4){
+          Console.WriteLine("Середа");
+      }
+      if(weekDay == 5){
+          Console.WriteLine("Четвер");
+      }
+      if(weekDay == 6){
+          Console.WriteLine("П'ятниця");
+      }
+  }
 }
+
+
